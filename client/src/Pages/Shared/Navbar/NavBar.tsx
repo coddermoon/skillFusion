d97464479c74f01cb7bcd4e53950ccef94d247ca
Mahamodul Hasan Moon <coddermoon@gmail.com>
2023-01-08 03:37:58 +0600

    done navbar

--- client/src/Pages/Shared/Navbar/NavBar.tsx
+++ client/src/Pages/Shared/Navbar/NavBar.tsx
@@ -1,10 +1,25 @@
-
+import logo  from '../../../assets/images/logo.png'
+import {NavLink} from 'react-router-dom'
 
 const NavBar = () => {
     return (
-        <div>
-            Navbar
-        </div>
+        <nav className="bg-gray-900">
+           <div className="container mx-auto  text-white flex justify-between items-center">
+            <div className="logo">
+            <img className='h-[80px]' src={logo} alt="" />
+            </div>
+            <div className="menus">
+            <NavLink className=' mx-5 font-semibold text-lg' to='/login'>Home</NavLink>
+            <NavLink className=' mx-5 font-semibold text-lg' to='/login'>courses</NavLink>
+            <NavLink className=' mx-5 font-semibold text-lg' to='/login'>About us</NavLink>
+            <NavLink className=' mx-5 font-semibold text-lg' to='/login'>contact us</NavLink>
+
+
+<NavLink className='bg-primary px-4 py-2 rounded' to='/login'>Login</NavLink>
+
+            </div>
+           </div>
+        </nav>
     );
 };
 
